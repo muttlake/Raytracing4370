76513ac5036cf22a183efe5c5566c3803be8c434
muttlake <shepbop@gmail.com>
2017-12-04 13:40:29 -0600

    Got triangle to show up.

--- Book_Clone/main.cpp
+++ Book_Clone/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "sphere.h"
 #include "plane.h"
+#include "triangle.h"
 #include "hitable_list.h"
 #include "float.h"
 #include "camera.h"
@@ -88,12 +89,8 @@ hitable *random_scene() {
 hitable *test_scene() {
     int n = 1;
 
-    texture *checker = new checker_texture( new constant_texture( vec3(0.1, 0.1, 0.1)), new constant_texture( vec3(0.9, 0.9, 0.9)), 0.10);
-
-    float plane_angle = 30*M_PI/180;
     hitable **list = new hitable*[n+1];
-    list[0] = new xy_plane(-1000.0, new lambertian(new constant_texture(vec3(0.8, 0.1, 0.1))));
-    //list[0] = new sphere(vec3(0,0,-10000), 10000, new lambertian(checker));
+    list[0] = new triangle(vec3(-10, 0, 0), vec3(0, 0, 10), vec3(10, 0, 0), 0.5, 0.5, new lambertian(new constant_texture(vec3(0.8, 0.1, 0.1))));
 
     return new hitable_list(list,n);
 }
@@ -122,7 +119,8 @@ hitable *project_scene() {
     //list[1] = new sphere(vec3(15, 2, 6), 6.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
     list[1] = new sphere(vec3(15, 2, 6), 6.0, new texture_metal(red_checker, 0.0));
     //list[1] = new sphere(vec3(15, 2, 6), 6.0, new texture_metal(loadSphereCheckerTexture(), 0.0));
-    list[2] = new sphere(vec3(2, -5, 5.5), 5.5, loadBrickTexture());
+    //list[2] = new sphere(vec3(2, -5, 5.5), 5.5, loadBrickTexture());
+    list[2] = new triangle(vec3(-5, 0, 0), vec3(0, 0, 5), vec3(5, 0, 0), 0.5, 0.5, new lambertian(new constant_texture(vec3(0.8, 0.1, 0.1))));
     list[3] = new sphere(vec3(-8, -16, 5), 5.0, new dielectric(1.5));
     //list[3] = new sphere(vec3(2, -5, 5.5), 5.5, new lambertian( new constant_texture(vec3(0.7, 0.3, 0.3))));
 
---
+++ Book_Clone/triangle.h
@@ -0,0 +1,66 @@
+#ifndef TRIANGLEH
+#define TRIANGLEH
+
+#include "hitable.h"
+
+
+class triangle: public hitable  {
+    public:
+        triangle() {}
+        triangle(vec3 _v0, vec3 _v1, vec3 _v2, float _u, float _v, material *mat) : v0(_v0), v1(_v1), v2(_v2), tex_u(_u), tex_v(_v), mp(mat) {};
+        virtual bool hit(const ray& r, float t0, float t1, hit_record& rec) const;
+        vec3 v0, v1, v2;
+	//vec3 n;
+	float tex_u, tex_v;
+        material  *mp;
+};
+
+
+bool triangle::hit(const ray& r, float t0, float t1, hit_record& rec) const
+{
+    const float EPSILON = 0.0000001; 
+
+    vec3 edge1, edge2, h, s, q;
+    float a,f,u,v;
+
+    edge1 = v1 - v0;
+    edge2 = v2 - v0;
+
+    h = cross(r.direction(), edge2);
+
+    a = dot(edge1, h);
+    if (a > -EPSILON && a < EPSILON)
+        return false;
+
+    f = 1/a;
+    s = r.origin() - v0;
+
+    u = f * dot(s, h);
+    if (u < 0.0 || u > 1.0)
+        return false;
+
+    q = cross(s, edge1);
+
+    v = f * dot(r.direction(), q);
+    if (v < 0.0 || u + v > 1.0)
+        return false;
+
+    // At this stage we can compute t to find out where the intersection point is on the line.
+    float t = f * dot(edge2, q);
+    if (t > EPSILON) // ray intersection
+    {
+        rec.p = r.point_at_parameter(t);
+        rec.u = u;
+        rec.v = v;
+        rec.t = t;
+        rec.mat_ptr = mp;
+        return true;
+    }
+    else // This means that there is a line intersection but not a ray intersection.
+        return false;
+}
+
+
+#endif
+
+
