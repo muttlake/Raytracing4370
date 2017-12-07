e278e1b7256b7d05445b2e1a98b77e590ff7863d
muttlake <shepbop@gmail.com>
2017-12-06 18:06:02 -0600

    Tried to add lights, not working, do not know if other files need to be changed besides plane.h and material.h, will abandon effort now and wrap up project.

--- Book_Clone/main.cpp
+++ Book_Clone/main.cpp
@@ -36,6 +36,7 @@ texture* loadSphereCheckerTexture()
 	return new image_texture(tex_data, nx, ny);
 }
 
+// Original color function
 vec3 color(const ray& r, hitable *world, int depth) {
     hit_record rec;
     if (world->hit(r, 0.001, MAXFLOAT, rec))
@@ -61,37 +62,23 @@ vec3 color(const ray& r, hitable *world, int depth) {
     }
 }
 
-
-hitable *random_scene() {
-    int n = 500;
-    hitable **list = new hitable*[n+1];
-    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(new constant_texture(vec3(0.5, 0.5, 0.5))));
-    int i = 1;
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            float choose_mat = drand48();
-            vec3 center(a+0.9*drand48(),0.2,b+0.9*drand48()); 
-            if ((center-vec3(4,0.2,0)).length() > 0.9) { 
-                if (choose_mat < 0.8) {  // diffuse
-                    list[i++] = new sphere(center, 0.2, new lambertian(new constant_texture(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48()))));
-                }
-                else if (choose_mat < 0.95) { // metal
-                    list[i++] = new sphere(center, 0.2,
-                            new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())),  0.5*drand48()));
-                }
-                else {  // glass
-                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
-                }
-            }
-        }
-    }
-
-    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
-    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
-    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
-
-    return new hitable_list(list,i);
-}
+////Color function for lights
+//vec3 color(const ray& r, hitable *world, int depth) {
+//    hit_record rec;
+//    if (world->hit(r, 0.001, MAXFLOAT, rec)) 
+//    { 
+//        ray scattered;
+//        vec3 attenuation;
+//        vec3 emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+//        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) 
+//             return emitted + attenuation*color(scattered, world, depth+1);
+//             //return attenuation;
+//        else 
+//            return emitted;
+//    }
+//    else 
+//        return vec3(0,0,0);
+//}
 
 triangle* loadTeapotObj() {
 	objl::Loader loader;
@@ -234,8 +221,6 @@ triangle* loadTeapotObj() {
 }
 
 
-
-
 hitable *test_scene() {
     int n = 1;
 
@@ -246,6 +231,21 @@ hitable *test_scene() {
     return new hitable_list(list,n);
 }
 
+
+hitable *lighted_spheres_project_scene() {
+    int n = 5;
+    texture *red_checker = new checker_texture( new constant_texture( vec3(0.2, 0.1, 0.1)), new constant_texture( vec3(0.7, 0.1, 0.1)), 0.50);
+    texture *plane_checker = new plane_checker_texture( new constant_texture( vec3(0.5, 0.5, 0.5)), new constant_texture( vec3(0.1, 0.1, 0.1)), -30.0);
+
+    hitable **list = new hitable*[n+1];
+    list[0] = new xy_plane(0.0, new texture_metal(plane_checker, 0.0));
+    list[1] = new sphere(vec3(15, 2, 6), 6.0, new texture_metal(red_checker, 0.0));
+    list[2] = new sphere(vec3(2, -5, 5.5), 5.5, loadBrickMaterial());
+    list[3] = new sphere(vec3(-8, -16, 5), 5.0, new dielectric(1.5));
+    list[4] = new yz_plane(40, new diffuse_light(new constant_texture(vec3(40,40,40))));
+    //list[4] = new yz_rect(-70, 1000, 0, 3000, 40, new diffuse_light(new constant_texture(vec3(40,40,40))));
+    return new hitable_list(list,n);
+}
 
 hitable *spheres_only_project_scene() {
     int n = 4;
@@ -327,6 +327,7 @@ int main(int argc, char *argv[]) {
     hitable *world;
     //world = test_scene();
     world = spheres_only_project_scene();
+    //world = lighted_spheres_project_scene();
     //world = first_triangle_project_scene();
     //world = project_scene();
 
--- Book_Clone/plane.h
+++ Book_Clone/plane.h
@@ -36,49 +36,40 @@ bool xy_plane::hit(const ray& r, float t0, float t1, hit_record& rec) const {
     return true;
 }
 
+class yz_plane: public hitable  {
+    public:
+        yz_plane() {}
+        yz_plane(float _k, material *mat) : k(_k), mp(mat) {};
+        //yz_plane(float _y0, float _y1, float _z0, float _z1, float _k, material *mat) : y0(_y0), y1(_y1), z0(_z0), z1(_z1), k(_k), mp(mat) {};
+        virtual bool hit(const ray& r, float t0, float t1, hit_record& rec) const;
+        //virtual bool bounding_box(float t0, float t1, aabb& box) const {
+        //       box =  aabb(vec3(x0,y0, k-0.0001), vec3(x1, y1, k+0.0001));
+        //       return true; }
+        material  *mp;
+        //float y0, y1, z0, z1, k;
+        float k;
+};
 
-//
-//class xy_plane: public hitable  {
-//    public:
-//        xy_plane() {}
-//        xy_plane(vec3 n, float d, material *m) : normal(n), distance(d), mat_ptr(m)  {};
-//        virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
-//        vec3 normal;
-//        float distance;
-//        material *mat_ptr;
-//};
-//
-//bool xy_plane::hit(const ray& r, float t_min, float t_max, hit_record& rec) const
-//{
-//    //ray_direction
-//    double a = dot(r.direction(), normal);
-//
-//    if (a > 1e-6)
-//    {
-//	vec3 perpPointOnPlane(0, 0, 0);
-//	perpPointOnPlane = r.origin() + (-1 * normal * distance);
-//	float t = dot(perpPointOnPlane, normal) / a;
-//	
-//	if (t >= 0)
-//	{
-//		rec.t = t;
-//		rec.p = perpPointOnPlane;
-//		rec.normal = normal;
-//		rec.mat_ptr = mat_ptr;
-//		return true;
-//	}
-//	
-//    }
-//    return false;
-//}
-//
-//struct hit_record
-//{
-//    float t;  
-//    vec3 p;
-//    vec3 normal; 
-//    material *mat_ptr;
-//};
+
+bool yz_plane::hit(const ray& r, float t0, float t1, hit_record& rec) const {
+    float t = (k-r.origin().x()) / r.direction().x();
+    if (t < t0 || t > t1)
+        return false;
+    float y = r.origin().y() + t*r.direction().y();
+    float z = r.origin().z() + t*r.direction().z();
+    //if (y < y0 || y > y1 || z < z0 || z > z1) 
+    //    return false;
+    //rec.u = (y-y0)/(y1-y0);
+    //rec.v = (z-z0)/(z1-z0); 
+    rec.u = 0.5;
+    rec.v = 0.5;
+    rec.t = t;
+    rec.mat_ptr = mp;
+    rec.p = r.point_at_parameter(t);
+    rec.normal = vec3(1, 0, 0);
+    return true;
+}
+
 
 #endif
 
